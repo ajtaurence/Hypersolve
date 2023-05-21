159767cbaca02461e1a2b5b5fc6729242ecb2cec
Anderson Taurence <50839724+ajtaurence@users.noreply.github.com>
2023-05-20 23:56:20 -0400

    updated build script

--- build.bat
+++ build.bat
@@ -1,14 +1,15 @@
-@echo Generating data . . .
-@md const_data >NUL 2>&1
-@cargo test generate --features gen-const-data >NUL 2>&1
+@ECHO off
+echo Generating data . . .
+md const_data >NUL 2>&1
+cargo test generate --features gen-const-data
 
-@if %ERRORLEVEL% == 0 goto :compile
-@echo "Error encountered during data generation."
-@goto :end
+if %ERRORLEVEL% == 0 goto :compile
+echo Error encountered during data generation.
+goto :end
 
 :compile
-@echo Compiling Hypersolve . . .
-@cargo build --features "" --release
+echo Compiling Hypersolve . . .
+cargo build --features "" --release
 
 :end
-@pause
+pause
